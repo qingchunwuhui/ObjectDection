b6a7d3f3ead093a5e1a33a25a161dccd847d468b
wangxiaofeng <123@qq.com>
2018-06-12 11:46:11 +0800

    finish HOG

--- HOG-SVM/opencv_HOG_SVM/HOG+SVM.cpp
+++ HOG-SVM/opencv_HOG_SVM/HOG+SVM.cpp
@@ -7,7 +7,8 @@
 using namespace cv;
 using namespace std;
 
-#define  FILEPATH "E:\\Learning Material\\OpenCV\\人脸检测数据库\\行人检测数据\\HOG测试程序\\Pedestrians64x128\\"
+//添加相对路径不行！
+#define  FILEPATH "E:\\VS_Programming\\OpenCV Program\\openCV-haar-boosted\\ObjectDection\\HOG-SVM\\opencv_HOG_SVM\\Pedestrians64x128\\"
 
 ///////////////////////////////////HOG+SVM识别方式2///////////////////////////////////////////////////  
 void Train()
@@ -72,9 +73,10 @@ void Train()
 		double time1 = getTickCount();
 		hog.compute(trainImage, descriptors);//这里可以设置检测窗口步长，如果图片大小超过64×128，可以设置winStride
 		double time2 = getTickCount();
-		double elapse_ms = (time2 - time1) * 1000 / getTickFrequency();
-		std::cout << "HOG dimensions:" << descriptors.size() << endl;
-		std::cout << "Compute time:" << elapse_ms << endl;
+		double elapse_ms = (time2 - time1)*1000 / getTickFrequency();   //返回的单位是秒
+		//std::cout << "HOG dimensions:" << descriptors.size() << endl;
+		//std::cout << "Compute time:" << elapse_ms << endl;
+		std::cout << "HOG Pic:" << i << "--->" << elapse_ms << endl;
 
 		//保存到特征向量矩阵中
 		for (vector<float>::size_type j = 0; j <= descriptors.size() - 1; ++j)
@@ -104,7 +106,7 @@ void Train()
 	/*
 	SVM训练完成后得到的XML文件里面，有一个数组，叫做support vector，还有一个数组，叫做alpha,有一个浮点数，叫做rho;
 	将alpha矩阵同support vector相乘，注意，alpha*supportVector,将得到一个行向量，将该向量前面乘以-1。之后，再该行向量的最后添加一个元素rho。
-	如此，变得到了一个分类器，利用该分类器，直接替换opencv中行人检测默认的那个分类器（cv::HOGDescriptor::setSVMDetector()），
+	如此，便得到了一个分类器，利用该分类器，直接替换opencv中行人检测默认的那个分类器（cv::HOGDescriptor::setSVMDetector()），
 	*/
 	//获取支持向量机：矩阵默认是CV_32F
 
@@ -138,8 +140,6 @@ void Train()
 	fprintf(fp, "%f", rho);
 
 	fclose(fp);
-
-
 }
 
 // 使用训练好的分类器识别
@@ -170,8 +170,8 @@ void Detect()
 
 	//设置HOG
 	HOGDescriptor hog;
-	hog.setSVMDetector(detector);// 使用自己训练的分类器
-	//hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());//可以直接使用05 CVPR已训练好的分类器,这样就不用Train()这个步骤了
+	//hog.setSVMDetector(detector);// 使用自己训练的分类器
+	hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());//可以直接使用05 CVPR已训练好的分类器,这样就不用Train()这个步骤了
 
 	namedWindow("people detector", 1);
 
@@ -253,6 +253,151 @@ void HOG_SVM2()
 	//如果使用05 CVPR提供的默认分类器，则不需要Train(),直接使用Detect检测图片
 	Train();
 	Detect();
+}
+
+///////////////////////////////////HOG+SVM识别方式1///////////////////////////////////////////////////
+void HOG_SVM1()
+{
+	////////////////////////////////读入训练样本图片路径和类别///////////////////////////////////////////////////
+	//图像路径和类别
+	vector<string> imagePath;
+	vector<int> imageClass;
+	int numberOfLine = 0;
+	string buffer;
+	ifstream trainingData(string(FILEPATH) + "TrainData.txt", ios::in);
+	unsigned long n;
+
+	while (!trainingData.eof())
+	{
+		getline(trainingData, buffer);
+		if (!buffer.empty())
+		{
+			++numberOfLine;
+			if (numberOfLine % 2 == 0)
+			{
+				//读取样本类别
+				imageClass.push_back(atoi(buffer.c_str()));
+			}
+			else
+			{
+				//读取图像路径
+				imagePath.push_back(buffer);
+			}
+		}
+	}
+	trainingData.close();
+
+
+	////////////////////////////////获取样本的HOG特征///////////////////////////////////////////////////
+	//样本特征向量矩阵
+	int numberOfSample = numberOfLine / 2;
+	Mat featureVectorOfSample(numberOfSample, 3780, CV_32FC1);//矩阵中每行为一个样本
+
+															  //样本的类别
+	Mat classOfSample(numberOfSample, 1, CV_32SC1);
+
+	//开始计算训练样本的HOG特征
+	for (vector<string>::size_type i = 0; i <= imagePath.size() - 1; ++i)
+	{
+		//读入图片
+		Mat src = imread(imagePath[i], -1);
+		if (src.empty())
+		{
+			cout << "can not load the image:" << imagePath[i] << endl;
+			continue;
+		}
+		cout << "processing" << imagePath[i] << endl;
+
+		//缩放
+		Mat trainImage;
+		resize(src, trainImage, Size(64, 128));
+
+		//提取HOG特征
+		HOGDescriptor hog(Size(64, 128), Size(16, 16), Size(8, 8), Size(8, 8), 9);
+		vector<float> descriptors;
+		hog.compute(trainImage, descriptors);//这里可以设置检测窗口步长，如果图片大小超过64×128，可以设置winStride
+		cout << "HOG dimensions:" << descriptors.size() << endl;
+
+		//保存特征向量矩阵中
+		for (vector<float>::size_type j = 0; j <= descriptors.size() - 1; ++j)
+		{
+			featureVectorOfSample.at<float>(i, j) = descriptors[j];
+		}
+
+		//保存类别到类别矩阵
+		//!!注意类别类型一定要是int 类型的
+		classOfSample.at<int>(i, 0) = imageClass[i];
+	}
+
+
+	///////////////////////////////////使用SVM分类器训练///////////////////////////////////////////////////	
+	//设置参数
+	//参考3.0的Demo
+	Ptr<ml::SVM> svm = ml::SVM::create();
+	svm->setKernel(ml::SVM::RBF);
+	svm->setType(ml::SVM::C_SVC);
+	svm->setC(10);
+	svm->setCoef0(1.0);
+	svm->setP(1.0);
+	svm->setNu(0.5);
+	svm->setTermCriteria(TermCriteria(CV_TERMCRIT_EPS, 1000, FLT_EPSILON));
+
+	//使用SVM学习         
+	svm->train(featureVectorOfSample, ml::ROW_SAMPLE, classOfSample);
+
+	//保存分类器
+	svm->save("Classifier.xml");
+
+
+	///////////////////////////////////使用训练好的分类器进行识别///////////////////////////////////////////////////
+	vector<string> testImagePath;
+	ifstream testData(string(FILEPATH) + "TestData.txt", ios::out);
+	while (!testData.eof())
+	{
+		getline(testData, buffer);
+		//读取
+		if (!buffer.empty())
+			testImagePath.push_back(buffer);
+
+	}
+	testData.close();
+
+	ofstream fileOfPredictResult(string(FILEPATH) + "PredictResult.txt"); //最后识别的结果
+	for (vector<string>::size_type i = 0; i <= testImagePath.size() - 1; ++i)
+	{
+		//读取测试图片
+		Mat src = imread(testImagePath[i], -1);
+		if (src.empty())
+		{
+			cout << "Can not load the image:" << testImagePath[i] << endl;
+			continue;
+		}
+
+		//缩放
+		Mat testImage;
+		resize(src, testImage, Size(64, 64));
+
+		//测试图片提取HOG特征
+		HOGDescriptor hog(cvSize(64, 64), cvSize(16, 16), cvSize(8, 8), cvSize(8, 8), 9);
+		vector<float> descriptors;
+		hog.compute(testImage, descriptors);
+		cout << "HOG dimensions:" << descriptors.size() << endl;
+
+		Mat featureVectorOfTestImage(1, descriptors.size(), CV_32FC1);
+		for (int j = 0; j <= descriptors.size() - 1; ++j)
+		{
+			featureVectorOfTestImage.at<float>(0, j) = descriptors[j];
+		}
+
+		//对测试图片进行分类并写入文件
+		int predictResult = svm->predict(featureVectorOfTestImage);
+		char line[512];
+		//printf("%s %d\r\n", testImagePath[i].c_str(), predictResult);
+		std::sprintf(line, "%s %d\n", testImagePath[i].c_str(), predictResult);
+		fileOfPredictResult << line;
+
+	}
+	fileOfPredictResult.close();
 }
 
 int main()
